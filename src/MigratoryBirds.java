d3603272ebd557c7bccb2eeba1422e9b803a824c
AdeKurr <kurniawanade24@gmail.com>
2022-03-01 12:31:57 +0700

    01/03/2022 Part 2

--- src/MigratoryBirds.java
+++ src/MigratoryBirds.java
@@ -7,28 +7,27 @@ public class MigratoryBirds {
         ArrayList<Integer> arr = new ArrayList<>();
         Collections.addAll(arr, 1, 4, 4, 4, 5, 3);
 
-        // Collections.sort(arr);
-        // // [1, 3, 4, 4, 4, 5]
-        // int firstNum = 0;
-        // int secondNum = 0;
+        Collections.sort(arr);
+        // [1, 3, 4, 4, 4, 5]
+        int firstNum = 0;
+        int secondNum = 0;
 
-        // HashMap<Integer, Integer> birb = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> birb = new HashMap<Integer, Integer>();
 
-        // for (int i = 0; i < arr.size(); i++) {
-        //     int tempSum = 0;
-        //     firstNum = arr.get(i);
-        //     for (int j = i; j < arr.size(); j++) {
-        //         secondNum = arr.get(j);
-        //         if (firstNum == secondNum) {
-        //             tempSum++;
-        //         }
+        for (int i = 0; i < arr.size(); i++) {
+            int tempSum = 0;
+            firstNum = arr.get(i);
+            for (int j = i; j < arr.size(); j++) {
+                secondNum = arr.get(j);
+                if (firstNum == secondNum) {
+                    tempSum++;
+                }
 
-        //     }
-        //     birb.
-        //     birb.put(firstNum, tempSum);
-        //     System.out.println(birb);
+            }
+            birb.put(firstNum, tempSum);
+            System.out.println(birb);
 
-        // }
+        }
         // System.out.println(birb);
     }
 }
