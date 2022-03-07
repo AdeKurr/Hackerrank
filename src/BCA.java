a930f1cbcb8b63d3e1e3bbb9834e0738d889ce90
AdeKurr <Kurniawanade24@gmail.com>
2022-03-07 19:26:12 +0700

    07/03/2022

--- src/BCA.java
+++ src/BCA.java
@@ -1,15 +1,19 @@
+import java.util.Scanner;
 
 public class BCA {
     public static void main(String[] args) {
-        // Scanner input = new Scanner (System.in);
-        int inputUser = 5;
-        int repetition = 5;
+        Scanner input = new Scanner (System.in);
+        System.out.println("Masukan tinggi segitiga:");
+        int inputUser = input.nextInt();
+        System.out.println("Masukkan jumlah segitiga:");
+        int repetition = input.nextInt();
+        
 
         for (int i = 0; i < inputUser; i++) {
             for (int rep = 0; rep < repetition; rep++) {
                 for (int space = inputUser; space > i + 1; space--) {
                     System.out.print(" ");
-                }
+                } 
                 for (int num = 0; num < (i * 2) + 1; num++) {
                     System.out.print("#");
                 }
@@ -20,5 +24,6 @@ public class BCA {
 
             System.out.println();
         }
+        input.close();
     }
 }
